358ce5d45cab7ce207acf6d431634dca3e61fdbf
Cory Riley <ceriley23@gmail.com>
2022-08-07 19:21:24 -0400

    final push, all working

--- index.js
+++ index.js
@@ -238,11 +238,11 @@ Using the miles function below do the following:
 3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
-  /*add your code here*/
+function miles(kilos){
+  return kilos * 0.621371
 }
 
-
+console.log('task 5a:', miles(7))
 
 //Task 5b - Feet to CM
 /*
@@ -252,11 +252,11 @@ Using the feet function below do the following:
 3. Return number of feet
 */
 
-function feet(/*add your code here*/){
-  /*add your code here*/
+function feet(cm){
+  return cm / 30.48;
 }
 
-
+console.log('task 5b:', feet(25))
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
@@ -272,10 +272,12 @@ Using the annoyingSong function below do the following:
 4. Each time the annoyingSong is run from this loop, it should console.log the string that was returned. 
 */
 
-function annoyingSong(/*add your code here*/){
-      /*add your code here*/
+function annoyingSong(bottle){
+      for (let i = bottle; i > 0; i--){
+        return `${i} bottles of soda on the wall, ${i} bottles of soda, take one down pass it around ${i-1} bottles of soda on the wall`
+      }
 }
-
+console.log('task 6:', annoyingSong(8))
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
@@ -292,10 +294,20 @@ Using the grade function below do the following:
  below should return 'you got an F'
 */
 
-function grade(/*Your Code here */){
-/*Your Code here */
+function grade(number){
+  if (number >= 90){
+    return 'you got an A';
+  }else if (number >= 80){
+    return 'you got a B';
+  }else if (number >= 70){
+    return 'you got a C';
+  }else if (number >= 60){
+    return 'you got a D';
+  }else if (number <= 59){
+    return 'you got an F'
+  }
 }
-
+console.log('final task:', grade(88))
 
 
 /*💪💪💪💪💪💪💪💪💪💪 Stretch 💪💪💪💪💪💪💪💪💪💪*/
